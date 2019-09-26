84e6c4ed60d824ed027d73340f51d77dbf93b846
Jason Schuchardt <jargon.schuchardt@gmail.com>
2019-09-26 14:39:44 -0700

    added some shell scripts

---
+++ clean.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# clean.sh
+# Copyright (C) 2019 jasonsch <jasonsch@walnut>
+#
+# Distributed under terms of the MIT license.
+#
+
+
+rm *~
+
+
---
+++ update.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+# update.sh
+# Copyright (C) 2019 jasonsch <jasonsch@walnut>
+#
+# Distributed under terms of the MIT license.
+#
+
+git pull
+
+chmod 644 *.html *.css *.js 
+
